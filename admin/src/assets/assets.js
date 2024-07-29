4d654e3930d692b0b5e323a4de7b58c19eefe62a
Sidharth Kumar <95740490+Sidharthkrsharma@users.noreply.github.com>
2024-07-30 03:24:29 +0530

    Update assets.js

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -5,6 +5,8 @@ import parcel_icon from './parcel_icon.png'
 import profile_image from './profile_image.png'
 import upload_area from './upload_area.png'
 
+export const url = 'https://foodomania-backend.onrender.com'
+
 export const assets ={
     logo,
     add_icon,
@@ -14,4 +16,3 @@ export const assets ={
     parcel_icon
 }
 
-//export const url = 'http://localhost:4000'
